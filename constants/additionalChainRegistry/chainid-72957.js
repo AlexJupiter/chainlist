f7c2e5dbd5ebc7d189ed4e01cecb992543984927
Alex Jupiter <alex@alexjupiter.com>
2026-04-29 16:05:10 +0100

    Add Rayls Mainnet configuration

---
+++ constants/additionalChainRegistry/chainid-72957.js
@@ -0,0 +1,25 @@
+{
+  "name": "Rayls Mainnet",
+  "chain": "RLS",
+  "rpc": [
+    "https://mainnet-rayls.com",
+  ],
+  "faucets": [],
+  "nativeCurrency": {
+    "name": "USD Rayls",
+    "symbol": "USDr",
+    "decimals": 18
+  },
+  "features": [{ "name": "EIP155" }, { "name": "EIP1559" }],
+  "infoURL": "https://rayls.com",
+  "shortName": "RLS",
+  "chainId": 72957,
+  "networkId": 72957,
+  "icon": "rayls",
+  "explorers": [{
+    "name": "Rayls Mainnet Explorer",
+    "url": "https://rayls.explorer.com",
+    "icon": "rayls",
+    "standard": "EIP3091"
+  }]
+}
